40a1f9c566b191c089d61a01b8ff018c3e8f498b
tsukimizake <shomasd@gmail.com>
2015-12-10 12:29:30 +0900

    Initial Commit

---
+++ osx-dict/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  osx-dict
+//
+//  Created by admin_sho on 2015/12/10.
+//  Copyright © 2015年 tsukimizake. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
